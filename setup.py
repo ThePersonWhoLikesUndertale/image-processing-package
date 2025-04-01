6a9e7c0bd333d6ac1e5dfc8311dad481d606f9c6
TPWLU <sppaulisto972@gmail.com>
2025-04-01 17:33:49 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="image-processing-package",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="samuell-shoiti-pinto-assanuma",
+    author_email="sppaulisto972@gmail.com",
+    description="Software for processing images",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/ThePersonWhoLikesUndertale/image-processing-package.git"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
