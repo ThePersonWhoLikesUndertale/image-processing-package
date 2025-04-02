fff370abd8b70ffdee2c04003fc1cc95b689f0b4
TPWLU <sppaulisto972@gmail.com>
2025-04-02 15:48:38 -0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="image-processing-package",
+    name="image_processing_package",
     version="0.0.1",
     author="samuell-shoiti-pinto-assanuma",
     author_email="sppaulisto972@gmail.com",
     description="Software for processing images",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ThePersonWhoLikesUndertale/image-processing-package.git"
+    url="https://github.com/ThePersonWhoLikesUndertale/image-processing-package.git",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
