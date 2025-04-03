16ac33c08b979fb843b2c94efdd91afaac7b8bc7
TPWLU <sppaulisto972@gmail.com>
2025-04-03 16:16:51 -0300

    Update package name in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="image_processing_package",
+    name="image_processing",
     version="0.0.1",
     author="samuell-shoiti-pinto-assanuma",
     author_email="sppaulisto972@gmail.com",
